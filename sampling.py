fa0d9e8c418a2000de4e6b3b2e5010bbe1181d16
Ben Landon <benlandon@Bens-MacBook-Pro-3.local>
2018-08-09 21:18:51 -0400

    Added final part of exercise

--- sampling.py
+++ sampling.py
@@ -8,6 +8,8 @@ Created on Tue Aug  7 15:59:35 2018
 
 import numpy as np
 import time
+from scipy.stats import norm
+
 
 """
 This function uses rejection sampling to draw samples from a posterier distribution.
@@ -80,4 +82,56 @@ def imp_samples(Y,maxsamps = -1, maxtime=20, verbose = True):
         print('Additional post-processing time:', "{0:.4f}".format(time.time()-timemid), 'seconds')
     return samples, weights
     
+"""
+Now we do importance sampling with a smarter proposal distribution
+"""
+
+
+def imp_samples_normal(Y, maxsamps = -1, maxtime = 20, verbose = True):
+    samples = np.array([])
+    logweights = np.array([])
+    timein = time.time()
+    timeout = time.time()+maxtime
+    cont = True
+    
+    #calculate MLE:
+    zz = np.arange(0.001,0.999,0.001)
+    MLE = zz[np.argmax( logpx(Y, zz))]
+    var = 1/(Y[0]/((2+MLE)*(2+MLE))+(Y[1]+Y[2])/((1-MLE)*(1-MLE))+Y[3]/(MLE*MLE))
+    std = np.sqrt(var)
+    
+    while cont:
+        needsample = True
+        while needsample:
+            X = np.random.normal(loc=MLE,scale = std)
+            if (X > 0) & (X < 1 ):
+                needsample = False
+            if time.time() > timeout:
+                needsample = False
+        if (X > 0 ) & (X<1) :
+            samples = np.append(samples, X)
+            logweights = np.append(logweights, logpx(Y,X)-np.log(norm.pdf(X,loc=MLE,scale=std)))
+            if (maxsamps >0) & (samples.size > maxsamps):
+                cont = False
+                print('Maximum samples reached')
+            if time.time() > timeout:
+                cont = False
+            
+    if samples.size > 0:
+        timemid = time.time()
+        logweights = logweights - np.max(logweights)
+        weights = np.exp(logweights)
+        weights = weights / np.sum(weights)
+        ESS = np.power(np.linalg.norm(weights), -2)
+        if verbose:
+            print(samples.size, 'samples were generated with an ESS of', "{0:.2f}".format(ESS))
+            print('Additional post-processing time:', "{0:.4f}".format(time.time()-timemid), 'seconds')
+    else: 
+        print('No samples were found!')
+    return samples, weights
+
+def MLE(Y):
+    zz = np.arange(0.001,0.999,0.001)
+    pr = logpx(Y,zz)
+    return zz[np.argmax(pr)]
 
